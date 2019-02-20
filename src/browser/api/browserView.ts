302706c8bca7b79106c42e66d7c04ebe83c1adbd
Pierre Baize <pierre@openfin.co>
2019-02-20 10:02:48 -0500

    workingish

--- src/browser/api/browserView.ts
+++ src/browser/api/browserView.ts
@@ -1,12 +1,9 @@
 import { BrowserWindow, BrowserView } from 'electron';
+const convertToElectron = require('../convert_options').convertToElectron;
 
 
 export function addBrowserViewToWindow(options: any, win: BrowserWindow) {
-    const view = new BrowserView({
-        webPreferences: {
-            nodeIntegration: false
-        }
-    });
+    const view = new BrowserView(convertToElectron({}, false));
     view.webContents.loadURL(options.url);
     view.setBounds(options.bounds);
     view.setAutoResize(Object.assign({width: true, height: true}, options.autoResize));
